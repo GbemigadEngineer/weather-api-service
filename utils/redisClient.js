0b1dd37ed3612baba4d1bb9ee6d7f5ae434119b4
GbemigadEngineer <samueloduwolegbolahan@gmail.com>
2025-08-07 13:09:05 +0100

    made additional changes to redis client

--- utils/redisClient.js
+++ utils/redisClient.js
@@ -3,10 +3,7 @@ const redis = require("redis");
 // Create Redis client for Memurai running locally
 
 const client = redis.createClient({
-  socket: {
-    host: process.env.REDIS_URL || "127.0.0.1",
-    port: 6379,
-  },
+  url: process.env.REDIS_URL || "127.0.0.1",
 });
 
 client.on("error", (err) => console.error("Redis Client Error:", err));
